9d61a1db5c8e5f6a0e2f9cd3ded221d1087f4035
maxefimov <efimov.academy.top@gmail.com>
2023-02-04 16:44:05 +0300

    Добавьте файлы проекта.

---
+++ Wpf3DGraphicsApp/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Wpf3DGraphicsApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
